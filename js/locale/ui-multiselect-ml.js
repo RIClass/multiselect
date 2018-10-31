6c6169c5d3449f8421179868aeac6a17d27d953d
Sagar V <sagarvd01@users.noreply.github.com>
2018-10-31 23:49:07 +0530

    Update and rename ui-multiselect-fr.js to ui-multiselect-ml.js

---
+++ js/locale/ui-multiselect-ml.js
@@ -0,0 +1,11 @@
+/**
+ * Localization strings for the UI Multiselect widget
+ *
+ * @locale ml, ml-ML, ml-IN
+ */
+
+$.extend($.ui.multiselect.locale, {
+	addAll:'ഐറ്റം ആഡ് ചെയ്യുക',
+	removeAll:'ഐറ്റം റിമോവ് ചെയ്യുക',
+	itemsCount:'ഐറ്റം സെലക്ട് ചെയ്തു'
+});
